9840277d55258322be8fcf911bcfa324e940880c
Olivier Paspuel <olivier_paspuel@hotmail.com>
2024-05-23 14:31:30 -0500

    tables n for loops

--- main.lua
+++ main.lua
@@ -10,20 +10,35 @@ function love.load()
     -- called only once at the beginning of the game;
     delta = 0
     positionX = 5;
-    name = "vier"
+    name = "vier";
+    names = {};
+    table.insert(names, "vier");
+    table.insert(names, "fünf");
+    table.insert(names, "sechs");
+    table.insert(names, "sieben");
 end
 
 function love.update(dt)
     delta = dt;
-    positionX = positionX + 40*dt;
-    -- runs every frame;
-    -- function love.draw()
-    --     love.graphics.print(dt, 400, 300)
+    
+    -- if positionX < 800 then
+    --     positionX = positionX + 100*dt;
     -- end
+    -- left and right movement
+    if(love.keyboard.isDown("right")) then
+        positionX = positionX + 100*dt;
+    elseif(love.keyboard.isDown("left")) then
+        positionX = positionX - 100*dt;
+    end
 end
 
 function love.draw()
     love.graphics.print("Hello, " .. name, 400, 300)
     love.graphics.print(delta, 400, 400)
     drawCircle(positionX, 400, 50);
+    for i,name in ipairs(names) do
+        love.graphics.print(name, 250, 50 + i*50)
+    end
+    love.graphics.print("amount of names in table: " .. #names, 400, 250);
 end
+
